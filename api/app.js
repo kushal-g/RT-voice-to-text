2d38430b1e28159db62c3f035414c9f67f109add
Kushal Garg <kushalgarg2000@gmail.com>
2020-04-24 05:37:57 +0530

    Integrates Google STT API and replaces MediaRecorder with recorder-js library

--- api/app.js
+++ api/app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const speech = require('@google-cloud/speech');
+
 const chalk = require('chalk');
 
-let app = express();
+const app = express();
+
+// Creates a client
+const client = new speech.SpeechClient();
 
 const io = require('socket.io')(process.env.SOCKET_PORT);
 
@@ -15,9 +20,32 @@ io.on('connection',(socket)=>{
     console.log(chalk.yellow('Client connected to server'));
 
     //socket emit/on
-    socket.on('startRecording',data=>{
+    socket.on('recording',async(data,fn)=>{
         console.log(chalk.magenta('Getting recording...'));
+        const audioString = data.toString('base64');
+       
         console.log(data);
+        const audio = {
+          content: audioString,
+        };
+
+        const config = {
+          encoding: 'LINEAR16',
+          sampleRateHertz: 48000,
+          languageCode: 'en-US',
+        };
+        const request = {
+          audio: audio,
+          config:config
+        };
+
+        const [response] = await client.recognize(request);
+        const transcription = response.results
+          .map(result => result.alternatives[0].transcript)
+          .join('\n');
+        console.log(`Transcription: ${transcription}`);
+        fn(transcription)
+
     })
 
 })
--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,54 @@
 import React from 'react';
 import socketIOClient from "socket.io-client";
-
-
+import Recorder from 'recorder-js';
 import './App.css';
 
 class App extends React.Component{
   
   static socket;
+  static recorder;
   constructor(props){
     super(props)
     this.socket = socketIOClient('http://127.0.0.1:80')
     this.state = {
-      notSupported:false
+      recording:false,
+      notSupported:false,
+      transcript:''
     }    
   }
 
   startRecording = async () =>{
     
-    const mimeType = 'audio/webm';
+    const audioContext =  new (window.AudioContext || window.webkitAudioContext)();
+    
+    const mimeType = 'audio/wav';
+
+    this.recorder = new Recorder(audioContext,{
+      'numChannels':1,
+      'mimeType':mimeType
+    })
     let chunks = [];
     try{
-      let stream = await navigator.mediaDevices.getUserMedia({audio:true,video:false});
+      let stream = await navigator.mediaDevices.getUserMedia({
+        audio:{
+          channelCount:1
+        },
+        video:false
+      });
       
-      let recorder = new MediaRecorder(stream,{type:mimeType});
       
-      recorder.start(1000);
-      recorder.addEventListener('dataavailable', event => {
+      this.recorder.init(stream)
+      
+      await this.recorder.start();
+      this.setState({recording:true});  
+      /* recorder.addEventListener('dataavailable', event => {
         if (typeof event.data === 'undefined') return;
           if (event.data.size === 0) return;
           console.log(event.data);
           this.socket.emit('recording',event.data);
         } 
-      );
+      ); */
       
-      document.getElementById('audio').srcObject = stream;
-      this.socket.emit('startRecording',stream);
     
     
     }catch(e){
@@ -43,14 +57,30 @@ class App extends React.Component{
     }
     
   }
+
+  stopRecording = async () =>{
+    
+    const {blob, buffer} = await this.recorder.stop()
+    console.log(blob)
+    document.getElementById('audio').src = URL.createObjectURL(blob);
+    this.setState({recording:false});
+    this.socket.emit('recording',blob,transcript=>{
+      this.setState({transcript:transcript})
+    });
+  }
+
   render(){
     
     return (
+      //TODO: Extract recorder as a separate component
       <div className="App">
         <h1>Real Time Text-to-Speech</h1>
         <div>
-          <audio id="audio" autoplay controls></audio>
-          <a href='#' onClick={()=>{this.startRecording()}}>Record</a>
+          <audio id="audio" autoPlay controls></audio>
+          <a href='#' onClick={()=>{this.state.recording?this.stopRecording():this.startRecording()}}>
+            {this.state.recording?'Stop':'Record'}
+          </a>
+          {this.state.transcript.length>0 && <p>{this.state.transcript}</p>}
           {this.state.notSupported && <p>Audio Streaming is not supported</p>}
         </div>
       </div>
